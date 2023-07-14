3d0407d1371ff638f6597459f0c1c72048e59f96
QADD <tudorps2@yahoo.com>
2023-07-14 18:47:43 +0300

    Added Mana Shield Accesory

---
+++ Common/Player/ManaShieldPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LUWeapons.Common.Player
+{
+    public class ManaShieldPlayer : ModPlayer
+    {
+        public bool HasManaShieldAcc;
+
+        // Always reset the accessory field to its default value here.
+        public override void ResetEffects()
+        {
+            HasManaShieldAcc = false;
+        }
+
+        public override void PostHurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit, int cooldownCounter)
+        {
+            if (!HasManaShieldAcc || Player.manaSick || Player.manaRegenBuff) return;
+            
+            var manaDiff = Player.statMana - (int)damage * 3;
+
+            if(manaDiff < 0)
+            {
+                Player.statLife += Player.statMana / 6;
+                Player.statMana = 0;
+                Player.manaRegenDelay = 0;
+                Player.manaRegenCount = 0;
+                Player.manaRegen = 0;
+                return;
+            }
+
+            Player.statMana = manaDiff;
+            Player.manaRegenDelay = 0;
+            Player.manaRegenCount = 0;
+            Player.manaRegen = 0;
+            Player.statLife += (int)damage / 2;
+        }
+    }
+}
---
+++ Content/Items/Accesory/ManaShield.cs
@@ -0,0 +1,39 @@
+﻿using LUWeapons.Common.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LUWeapons.Content.Items.Accesory
+{
+    public class ManaShield : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Use Mana to reduce damage taken by 50%\n" +
+                                "Mana Regeneration Drastically Decreased\n" +
+                                "Mana shield takes triple the non-reduced damage\n" +
+                                "The accesory has no effect if either Mana Sickness or Mana Regeneration Buff is active");
+
+            //CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<ManaShieldPlayer>().HasManaShieldAcc = true;
+            player.manaRegenBonus = -25;
+        }
+    }
+
+    
+}
